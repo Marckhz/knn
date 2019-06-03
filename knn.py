778a9f3ba432f57fc6b54402edbef7880bb2acdb
pixelMarck <marcohdes94i@gmail.com>
2019-06-02 18:26:06 -0600

    arrays

--- knn.py
+++ knn.py
@@ -4,7 +4,7 @@ import math
 
 from PIL import Image
 
-testdata = np.loadtxt("rsTrain.txt")
+trainingdata = np.loadtxt("rsTrain.txt")
 
 #img1 = Image.fromarray(testdata)
 #img1.show()
@@ -19,10 +19,16 @@ print(band1)
 print("shape", band1.shape)
 
 
-x = testdata[:,0:4]
+x = trainingdata[:,0:4]
 
 
-print(testdata.shape)
+
+
+print(trainingdata.shape)
+
+
+
+#print(distance)
 
 """
 k = 1
@@ -57,8 +63,50 @@ reshape_band2 = np.reshape(band2, (512, 512) )
 reshape_band3  = np.reshape(band3, (512, 512) )
 reshape_band4 = np.reshape(band4, (512, 512) )
 
+
+band_1_to_test = x[:,0]
+#print(band_1_to_test)
+
+#en esta parte estoy iterando
+#por el la primer colimna del dataset de entrenamiento
+#y esto sacando la distancia  del training set y el testset
+#ahora necesito sacar los k vecinos.
+#voy  a ordernar el array y tomar los k vecinos.
+#despues de eso que?
+
+distance = 0
+#distancias con indices
+
+index_and_distance = []
+for position, point in enumerate(band_1_to_test):
+    distance += np.square(reshape_band1 - point)
+    square_distance = np.sqrt(distance)
+    index_and_distance.append((position, square_distance ) )
+
+print(index_and_distance)
+
+#sorted_distance = np.sort(square_distance)
+
+#print("sorted distance ", sorted_distance)
+#print(sorted_distance.index)
+k = 1
+
+
+
+
+
+#for x in range(k):
+#   neighbors.append(sorted_distance[x])
+
+#print(neighbors) 
+
+
 #img = Image.fromarray(reshape_band4)
 #img.show()
+
+"""
+
+
 
 
 kolkata = reshape_band1 + reshape_band2 + reshape_band3 + reshape_band4
@@ -72,7 +120,7 @@ vector = []
 distance = 0
 i = 0
 for cell in np.nditer(x, order = 'C'):
-    distance = np.sqrt(np.square(kolkata - cell) )
+    distance = np.sqrt(np.square(reshape_band1[0] - cell) )
     #print(distance)
     #i+=1
     #if i == 50:
@@ -103,9 +151,7 @@ print(new_band1[:,0].shape )
 
 
 
-
-
-
+"""
 
 
 
